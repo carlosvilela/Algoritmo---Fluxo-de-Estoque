b99efd07dd05e3a89474483b56eb71db82352bef
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-02-08 18:00:20 -0200

    Create ATV01-Exercicio01.c

---
+++ Introducao-a-Programacao/Atividade-Supervisionada/ATV01-Exercicio01.c
@@ -0,0 +1,9 @@
+/*
+1- Faça um programa que leia seu nome e sua idade. Após duas linhas e exiba nome e idade separados por hífen.
+
+Exemplo:
+Digite seu nome >> Fulano
+Idade >> 30
+
+Fulano – 30 anos
+*/
