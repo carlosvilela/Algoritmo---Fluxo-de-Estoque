8974caf6acc7482596e7e521087c81233e658ac5
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-02-13 08:43:28 -0200

    Create exercicio_02.c

---
+++ Introducao-a-Programacao/Exercicios/Lista_1/exercicio_02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (int argc, char** argv[]){
+	
+	printf ("Carlos Vilela\n"); // o \n (barra invertida n) serve para pular linha
+	printf("Engenharia da Computacao");
+	
+	return 0;
+}
