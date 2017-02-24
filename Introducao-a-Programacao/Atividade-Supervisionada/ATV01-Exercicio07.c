1198da5656f67b8cf6824e4a3ec9e3133ef533f0
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-02-24 12:49:20 -0300

    Update ATV01-Exercicio07.c

--- Introducao-a-Programacao/Atividade-Supervisionada/ATV01-Exercicio07.c
+++ Introducao-a-Programacao/Atividade-Supervisionada/ATV01-Exercicio07.c
@@ -2,20 +2,21 @@
 7-Faça um algoritmo que leia o tempo de duração de um evento em uma fábrica expressa em segundos
 e mostre-o expresso em horas, minutos e segundos.
 */
-#include "stdlib.h"
+
+#include <stdio.h>
 
 int main (int argc, char** argv[]){
-	
-	int tempo, horas, minutos, segundos;
-		
-	printf("Insira o tempo de duracao em segundos: ");
-	scanf("%i", &tempo);
-	
-	horas = (tempo/60)/60;
-	minutos = (tempo-((horas*60)*60))/60;
-	segundos = tempo- ((horas*60*60)+(minutos*60));
-	
-	printf("%ih %im %is", horas, minutos, segundos);
-	
-	return 0;
+
+int tempo, horas, minutos, segundos; // usar inteiro para descartar o ponto flutuante do calculo
+
+printf("Insira o tempo de duracao em segundos: ");
+scanf("%d", &tempo);
+
+horas = (tempo/60) /60; // TempoTotal em minutos dividido por 60 para converter em horas
+minutos = (tempo - ((horas*60)*60)) /60; // TempoTotal em segundos subtraindo por horas em segundos e depois dividindo por 60 para converter em minutos
+segundos = tempo - ((horas*60*60)+(minutos*60)); // TempoTotal em segundos subtraindo por Horas em segundos somada com os minutos em segundos
+
+printf("\n\na duracao corresponde a %dh %dm %ds,", horas, minutos, segundos);
+
+return 0;
 }
