5735fd9620b5187f53c353d1a0c20ded2c015fed
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-02-24 12:47:56 -0300

    Update ATV01-Exercicio05.c

--- Introducao-a-Programacao/Atividade-Supervisionada/ATV01-Exercicio05.c
+++ Introducao-a-Programacao/Atividade-Supervisionada/ATV01-Exercicio05.c
@@ -5,21 +5,21 @@ Supondo que a percentagem do distribuidor seja de 28% e os impostos de 45%,
 escrever um programa em C que leia o custo de fábrica de um carro e escreva o custo ao consumidor. 
 */
 
-#include "stdlib.h"
+#include <stdio.h>
 
 int main (int argc, char** argv[]){
-	
-	float custo_consumidor, custo_fabrica, comissao_distribuidor, impostos;
-	
-	printf("Insira o custo de fabrica: ");
-	scanf("%f", &custo_fabrica);
-	
-	comissao_distribuidor = custo_fabrica*0.28;
-	impostos = custo_fabrica*0.45;
-	custo_consumidor = custo_fabrica+comissao_distribuidor+impostos;
-	
-	printf("O Custo do Consumidor e: R$ %.2f", custo_consumidor);
-	
-	return 0;
-	 
+
+float custo_consumidor, custo_fabrica, comissao_distribuidor, impostos;
+
+printf("Insira o custo de fabrica: ");
+scanf("%f", &custo_fabrica);
+
+comissao_distribuidor = custo_fabrica*0.28;
+impostos = custo_fabrica*0.45;
+custo_consumidor = custo_fabrica+comissao_distribuidor+impostos;
+
+printf("\n\nO Custo do Consumidor e: R$ %.2f", custo_consumidor);
+
+return 0;
+
 }
