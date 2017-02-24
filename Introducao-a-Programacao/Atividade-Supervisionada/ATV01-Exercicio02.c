2ac2c28f7e9c9b2ed7d1d1b24119170944725295
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-02-24 12:46:06 -0300

    Update ATV01-Exercicio02.c

--- Introducao-a-Programacao/Atividade-Supervisionada/ATV01-Exercicio02.c
+++ Introducao-a-Programacao/Atividade-Supervisionada/ATV01-Exercicio02.c
@@ -3,18 +3,18 @@
 
 Use quatro casas decimais, suponha que o número informado pelo usuário seja maior que zero.
 */
-#include "stdlib.h"
+#include <stdio.h>
 
 int main (int argc, char** argv[]){
-	
-	float DistanciaKM, DistanciaCM;
-	
-	printf("Insira a distancia em km: ");
-	scanf("%f",&DistanciaKM);
-	
-	DistanciaCM = (DistanciaKM*1000); // km hm dam M cm dm mm
-	
-	printf("A Distancia %.4fkm e equivalente a %.4fcm", DistanciaKM, DistanciaCM);
-	
-	return 0;
+
+float DistanciaKM, DistanciaCM;
+
+printf("Insira a distancia em km: ");
+scanf("%f",&DistanciaKM);
+
+DistanciaCM = (DistanciaKM*100000); // km hm dam M dm cm mm
+
+printf("\n\nA Distancia de %.4fkm e equivalente a %.4fcm", DistanciaKM, DistanciaCM);
+
+return 0;
 }
