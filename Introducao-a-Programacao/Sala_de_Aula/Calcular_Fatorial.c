46d61ed601596417904bafbebda3ebcc45841c0a
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-03-23 08:11:42 -0300

    Create Calcular_Fatorial.c

---
+++ Introducao-a-Programacao/Sala_de_Aula/Calcular_Fatorial.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv[]){
+	
+	int i,x,fat;
+	
+	printf ("Insira o numero: ");
+	scanf("%d", &x);
+	
+	i=1;
+	fat = 1;
+	
+	while (i<=x){
+		
+		fat = fat*(x-i+1);
+	printf("%d ", (x-i+1));
+		
+		
+				
+		i++;
+	}	
+	
+	printf("= %d", fat);
+	
+	
+	
+	return 0;
+}
