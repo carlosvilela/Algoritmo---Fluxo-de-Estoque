0f3c700a52f6bba6f543a17f8fa8f711170d0dda
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-08-23 10:32:02 -0300

    Create Exercicio_aula3.c

---
+++ Programacao Estruturada/Exercicio_aula3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#define max 10
+
+main(){
+
+    char num[max];
+
+    printf("Insira um valor em base decimal: ");
+    scanf("%s", &num);
+
+    int decimal = atoi(num);
+
+
+
+
+}
