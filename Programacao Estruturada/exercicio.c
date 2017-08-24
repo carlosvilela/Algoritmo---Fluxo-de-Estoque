ef960dd768eddbbcb7c636e75180d0958fc0803f
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-08-24 12:22:22 -0300

    Update exercicio.c

--- Programacao Estruturada/exercicio.c
+++ Programacao Estruturada/exercicio.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+
+
+void convert (int,int);
 
 
 main(){
 
-    int decimal,base,b, resto[16];
-    char valorBase[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    int decimal,b;
 
 
     printf("|| == >>>>>>>>> Conversor de Bases, de decimal para qualquer base <<<<<<<< == ||");
@@ -14,20 +17,42 @@ main(){
     printf("Insira a base de conversao limitada ate 16: ");
     scanf("%d", &b);
 
-    base = b;
+    convert(decimal, b);
+
+
+}
+
+void convert (int valor,int base){
+
+    int resto[16], b, numero;
+    char valorBase[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    char resposta[16];
+    numero = valor;
+
+
+    b = base;
 
     int i = 0;
-    while(decimal>0){
+    while(numero>0){
 
-        resto[i] = decimal%base;
-        decimal = decimal/b;
+        resto[i] = numero%base;
+        numero = numero/b;
+
+         resposta[i] = valorBase[resto[i]];
 
 
-        printf("%c", valorBase[resto[i]]);
 
     i++;
 
+    }
+
+
+
+    for (int y = (strlen(resposta)); y >=0; y--){
+
+         printf("%c", resposta[y]);
 
     }
+
 
 }
