3d51c3bb591f9d4edf8b5f3f773db5f878f7fbaf
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-10-28 12:40:46 -0200

    Create Lista5_exercicio01.c

---
+++ Estrutura_de_Dados/Lista5_exercicio01.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Elemento {
+ int chave;
+ struct Elemento *prox;
+} Elemento;
+
+struct Elemento *p, *prim, *h, *q;
+int i;
+
+main(){
+    system("cls");
+    prim = NULL;
+
+    p = (Elemento*) malloc(sizeof(Elemento));
+
+    h = p;
+
+    p->chave = 1;
+
+    for (i = 1; i<=3; i++){
+
+        q = (Elemento*) malloc(sizeof(Elemento));
+        q->chave = p->chave+2;
+        printf("%d , %d, %d\n", h->chave, p->chave, q->chave);
+        p = q;
+    }
+
+    printf("\n\n ->> %d, %d, %d\n", h->chave, p->chave, q->chave);
+
+}
